bbac993d05996ffbd1119890163864f5d6c6f252
Rahul Chatterjee <rahulc@deltecs.local>
2019-12-10 20:13:20 +0530

    Added 2 components contacts and favourites

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"contacts", component:ContactsComponent},
+  {path:"",redirectTo:"contacts",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    FavouritesComponent
   ],
   imports: [
     BrowserModule,
