9688c388507abce674572a1ab246e4f9fe97a8a1
Rahul Chatterjee <rahulc@deltecs.local>
2019-12-10 22:57:07 +0530

    created basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
 
 
 const routes: Routes = [
   {path:"contacts", component:ContactsComponent},
+  {path:"favourites", component:FavouritesComponent},
   {path:"",redirectTo:"contacts",pathMatch:"full"}
 ];
 
